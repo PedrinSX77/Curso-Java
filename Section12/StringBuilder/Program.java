7008785761631fc8d2ec76f8b26c489545fa032d
Pedro Henrique <pedro.h.morschbacher@gmail.com>
2026-09-24 01:49:32 -0300

    refactor(Section12): simplificar saida com IO.println e corrigir grafia em Program
    - Atualiza chamada de impressao de System.out.println para IO.println
    - Corrige grafia do titulo para 'Traveling to New Zealand'

--- Section12/StringBuilder/Program.java
+++ Section12/StringBuilder/Program.java
@@ -17,15 +17,15 @@ public class Program {
         LocalDateTime date1 = LocalDateTime.parse("21/06/2018 13:05:44", dateTimeFormatter);
         LocalDateTime date2 = LocalDateTime.parse("28/07/2018 23:14:44", dateTimeFormatter);
 
-        Post post1 = new Post(date1, "Traveling to New Zeland", "I'm going to visit this wonderful country!", 12);
+        Post post1 = new Post(date1, "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
         post1.addComment(comment1);
         post1.addComment(comment2);
         Post post2 = new Post(date2, "Good night guys", "See you tomorrow!", 5);
         post2.addComment(comment3);
         post2.addComment(comment4);
 
-        System.out.println(post1);
-        System.out.println();
-        System.out.println(post2);
+        IO.println(post1);
+        IO.println();
+        IO.println(post2);
     }
 }
